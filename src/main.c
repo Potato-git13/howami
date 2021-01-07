1a6268231619daceb717c9bae650686294634ec1
Potato Man <lanveble08@gmail.com>
2021-01-07 17:17:45 +0100

    removed the newlines from the computer feeliing strings and added . or ! at the end of the strings

--- src/main.c
+++ src/main.c
@@ -24,33 +24,34 @@ int main(){
     // get the precentage
     int ram_used_perc = (ram_used * 100) / (ram_total_out/1048576);
     // main will print out all of the info and the feeling. (when it's done the percentages and all of that will not be displayed)
-    printf("BAT: %d%%\n", bat_perc());
-    printf("RAM perc: %d%%\n", ram_used_perc);
+    //printf("BAT: %d%%\n", bat_perc());
+    //printf("RAM perc: %d%%\n", ram_used_perc);
     printf("%s", bat_rating(bat_perc()));
     printf("%s", ram_rating(ram_used_perc));
+    printf("\n");
     return 0;
 }
 
 const char *bat_rating(int bat_percent){
     static char bat_feeling[40];
     if (bat_percent<=40){
-        strcpy(bat_feeling, "I feel sleepy..\n");
+        strcpy(bat_feeling, "I feel sleepy.. ");
         total_feeling_rate += 1;
     }
     else if (bat_percent<=50 && bat_percent>40){
-        strcpy(bat_feeling, "I'm half fully energized\n");
+        strcpy(bat_feeling, "I'm half fully energized. ");
         total_feeling_rate += 2;
     }
     else if (bat_percent<=80 && bat_percent>50){
-        strcpy(bat_feeling, "I'm good with the battery\n");
+        strcpy(bat_feeling, "I'm good with the battery! ");
         total_feeling_rate += 3;
     }
     else if (bat_percent<=99 && bat_percent>80){
-        strcpy(bat_feeling, "I'm great with the battery!\n");
+        strcpy(bat_feeling, "I'm great with the battery! ");
         total_feeling_rate += 4;
     }
     else if (bat_percent==100){
-        strcpy(bat_feeling, "I'm fully energized!\n");
+        strcpy(bat_feeling, "I'm fully energized! ");
         total_feeling_rate += 5;
     }
     return bat_feeling;
@@ -59,23 +60,23 @@ const char *bat_rating(int bat_percent){
 const char *ram_rating(int ram_perc){
     static char ram_feeling[46];
     if(ram_perc==100){
-        strcpy(ram_feeling, "AAAAAAAAAAAAAAAAAAAAHHH my RAM is gonna FRY!\n");
+        strcpy(ram_feeling, "AAAAAAAAAAAAAAAAAAAAHHH my RAM is gonna FRY! ");
         total_feeling_rate += 1;
     }
     else if (ram_perc<=99 && ram_perc>80){
-        strcpy(ram_feeling, "My RAM is melting\n");
+        strcpy(ram_feeling, "My RAM is melting! ");
         total_feeling_rate += 2;
     }
     else if (ram_perc<=80 && ram_perc>50){
-        strcpy(ram_feeling, "My RAM is not being spared by the user\n");
+        strcpy(ram_feeling, "My RAM is not being spared by the user. ");
         total_feeling_rate += 3;
     }
     else if (ram_perc<=50 && ram_perc>30){
-        strcpy(ram_feeling, "The RAM is quite OK\n");
+        strcpy(ram_feeling, "The RAM is quite OK. ");
         total_feeling_rate += 4;
     }
     else if (ram_perc<=30 && ram_perc>=0){
-        strcpy(ram_feeling, "The RAM very cool\n");
+        strcpy(ram_feeling, "The RAM is very cool. ");
         total_feeling_rate += 5;
     }
     return ram_feeling;
